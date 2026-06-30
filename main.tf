a9e8ea9a0be79f374f59601f523b918447a28a16
Freddy ANdrés Lemus Barrera <sumel124@gmail.com>
2026-06-30 12:29:31 -0500

    Use module for security group

--- main.tf
+++ main.tf
@@ -16,18 +16,31 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "Freddy"
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "6.0.0"
+  name    = "blog_new_freddy"
+
+  vpc_id  = data.aws.vpc.default.id
+  ingress_rules =. ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  
+  egress_rules =. [all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group" "blog" {
   name        = "blog"
-  description = "Allow http and https in. Allow everything out"
+  descripcion = "Allow http and https in. Allow everything out"
 
-  vpc_id      = data.aws_vpc.default.id  
+  vpc_id      = data.aws.vpc.default.id  
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
@@ -35,7 +48,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  cldr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -45,18 +58,18 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  cldr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
 
 
 resource "aws_security_group_rule" "blog_http_out" {
-  type        = "egress"
+  type        = "engress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  cldr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
