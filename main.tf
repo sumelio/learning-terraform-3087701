26fb06853ff280d06a8815a6236c81c7aefdb77c
Freddy ANdrés Lemus Barrera <sumel124@gmail.com>
2026-06-30 12:06:09 -0500

    Fix typepo

--- main.tf
+++ main.tf
@@ -25,9 +25,9 @@ resource "aws_instance" "blog" {
 
 resource "aws_security_group" "blog" {
   name        = "blog"
-  descripcion = "Allow http and https in. Allow everything out"
+  description = "Allow http and https in. Allow everything out"
 
-  vpc_id      = data.aws.vpc.default.id  
+  vpc_id      = data.aws_vpc.default.id  
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
@@ -35,7 +35,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cldr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -45,18 +45,18 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cldr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
 
 
 resource "aws_security_group_rule" "blog_http_out" {
-  type        = "engress"
+  type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
-  cldr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
