a768a3eb7dad6bd67042db4b6c5d0a2f9ddba48c
Freddy ANdrés Lemus Barrera <sumel124@gmail.com>
2026-06-30 17:07:22 -0500

    Create a new dev vpc

--- main.tf
+++ main.tf
@@ -8,8 +8,22 @@ data "aws_ami" "app_ami" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  
+  name = "dev-vpc"
+  cidr = "10.0.0.0/16"
+  
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  
+  enable_nat_gateway = true
+  
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -17,7 +31,8 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [aws_security_group.blog.id]
-
+  
+  subnet_id = module.blog_vpc.public_subnets[0]
   tags = {
     Name = "Freddy"
   }
@@ -27,9 +42,9 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 5.0"   # was 6.0.0
 
+  vpc_id      = module.blog_vpc.vpc_id
   name        = "blog_new_freddy"
   description = "Allow http and https in, all out"
-  vpc_id      = data.aws_vpc.default.id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
