ed37b0a4039074c14fadcf29e7da9b0935abbb6f
Freddy ANdrés Lemus Barrera <sumel124@gmail.com>
2026-06-30 18:23:04 -0500

    delete unused module

--- main.tf
+++ main.tf
@@ -111,12 +111,6 @@ resource "aws_lb_target_group" "blog" {
   vpc_id   = module.blog_vpc.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
-}
-
 module "blog_autoscalling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.0.2"
