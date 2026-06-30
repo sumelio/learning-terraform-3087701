1aa3fa90ea7487292ebd1910b6cc53a2a187c555
Freddy ANdrés Lemus Barrera <sumel124@gmail.com>
2026-06-30 17:53:07 -0500

    load balance

--- main.tf
+++ main.tf
@@ -90,3 +90,44 @@ resource "aws_security_group_rule" "blog_http_out" {
 
   security_group_id = aws_security_group.blog.id
 }
+
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog_my-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  security_groups = [module.blog_sg.security_group_id]
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  listeners = {
+    blog-https = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_arn = aws_lib_target_group.blog.arn
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "aws_lb_target_group" "blog" {
+  name     = "blog"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.blog_vpc.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "blog" {
+  target_group_arn = aws_lb_target_group.blog.arn
+  target_id        = aws_instance.blog.id
+  port             = 80
+}
